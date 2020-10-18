79db8c7ef5ebd2129093bebe44c381cc7216ce27
Yash Mahalwal <yashmahalwal@gmail.com>
2020-10-19 01:13:32 +0530

    Updates

---
+++ component---src-pages-404-tsx-bb98f2ff7ea136b22024.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),r=t.n(a);e.default=function(){return r.a.createElement(r.a.Fragment,null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-bb98f2ff7ea136b22024.js.map

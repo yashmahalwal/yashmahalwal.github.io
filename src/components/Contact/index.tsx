8f9c2fee5c389522b7df136052c08f6cecbe3556
Yash Mahalwal <yashmahalwal@gmail.com>
2020-10-27 00:53:34 +0530

    color fixes

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,8 +1,16 @@
 import React from "react"
+import Spider from "../Spider"
 import classes from "./styles.module.scss"
 
 const Contact: React.FC = () => {
-  return <section className={classes.section}></section>
+  return (
+    <section className={classes.section}>
+      <h2 className="hidden">Contact details</h2>
+      <div className="container">
+        <img src="/28523.jpg" alt="" />
+      </div>
+    </section>
+  )
 }
 
 export default Contact
